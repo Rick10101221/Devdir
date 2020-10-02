d834887d6307f8629af0332c4039306b320fd975
Emaneliforp <kietchouam@gmail.com>
2020-10-01 17:08:51 -0700

    added search

--- public/js/search.js
+++ public/js/search.js
@@ -4,28 +4,28 @@ function search(searchedSkill){ //searchedSkill is array
     let arr = [];
     db.ref('profile/').once('value').then((snapshot)=>{
         users = snapshot.val();
-        console.log(users);
         for(let i = 0; i < searchedSkill.length; i++){
             for (const user in users) {
                 if(users[user]['skill'].includes(searchedSkill[i])){
-                    console.log("in");
                     if(result[user]){
                         result[user]['priority']++;
                     }
                     else{
-                        result[user] = {'priority': 1};
+                        result[user] = {'profile': users[user],'priority': 1};
                     }
                 }
             }
         }
+        for(const user in result){
+            if(arr[result[user]['priority']]){
+                arr[result[user]['priority']].push(result[user]['profile']);
+            }
+            else{
+                (arr[result[user]['priority']] = []).push(result[user]['profile']);
+            }
+        }
+        console.log(arr);
+        return arr;
     })
-    for(const user in result){
-        if(arr[result[user]] && arr[result[user]] > 0){
-            arr[result[user]].push(result[user]);
-        }
-        else{
-            (arr[result[user]] = []).push(result[user]);
-        }
-    }
-    return result;
+    
 }
