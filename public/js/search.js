2559744c506fc9f6be9705ab7d38d04ef53001f8
Emaneliforp <kietchouam@gmail.com>
2020-10-01 17:52:47 -0700

    modified search

--- public/js/search.js
+++ public/js/search.js
@@ -1,31 +1,43 @@
 let users = {};
 function search(searchedSkill){ //searchedSkill is array
     let result = {};
-    let arr = [];
+    let sort = [];
+    let fill = [];
     db.ref('profile/').once('value').then((snapshot)=>{
         users = snapshot.val();
         for(let i = 0; i < searchedSkill.length; i++){
             for (const user in users) {
-                if(users[user]['skill'].includes(searchedSkill[i])){
-                    if(result[user]){
-                        result[user]['priority']++;
-                    }
-                    else{
-                        result[user] = {'profile': users[user],'priority': 1};
+                if(users[user]['skill']){
+                    if(users[user]['skill'].includes(searchedSkill[i])){
+                        if(result[user]){
+                            result[user]['priority']++;
+                        }
+                        else{
+                            result[user] = {'profile': users[user],'priority': 1};
+                        }
                     }
                 }
             }
         }
         for(const user in result){
-            if(arr[result[user]['priority']]){
-                arr[result[user]['priority']].push(result[user]['profile']);
+            if(sort[result[user]['priority']]){
+                sort[result[user]['priority']].push(result[user]['profile']);
             }
             else{
-                (arr[result[user]['priority']] = []).push(result[user]['profile']);
+                (sort[result[user]['priority']] = []).push(result[user]['profile']);
             }
         }
-        console.log(arr);
-        return arr;
-    })
-    
+        let order = 0;
+        for(let i = sort.length-1; i >= 0 ; i--){
+            if(sort[i]){
+                if(fill[order]){
+                    fill[order].push(sort[i]);
+                } else{
+                    (fill[order] = []).push(sort[i]);
+                }
+                order++;
+            }
+        }
+    });
+    return fill;
 }
