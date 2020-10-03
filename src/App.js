596aa85be2fb0c90827a476231036e6da4dcdf72
Darien Tsai <smartdragondt@gmail.com>
2020-10-03 16:20:28 -0700

    firebase in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { requirePropFactory } from '@material-ui/core';
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
-import start from './db/test';
+//import {app, auth, db} from './db/test';
+var firebase = require('firebase');
+require('firebase/auth');
+require('firebase/database');
+require('dotenv').config();
+
+var app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId
+});
+
+var auth = firebase.auth();
+var db = firebase.database();
 
 class App extends React.Component {
   render(){
@@ -28,8 +45,9 @@ class App extends React.Component {
   }
 
   componentDidMount(){
-    var app = start();
     console.log(app);
+    console.log(auth);
+    console.log(db);
 
     // TODO firebase check authenticated 
     // TODO firebase get profile
--- src/db/test.js
+++ src/db/test.js
@@ -1,16 +1,24 @@
+require('dotenv').config();
 var firebase = require('firebase');
 require('firebase/auth');
 require('firebase/database');
 
-var start = () => {
-  return firebase.initializeApp({
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId
-  });
-}
+//console.log(process.env.apiKey);
 
-export default start;
+var app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId
+  });
+
+var auth = firebase.auth();
+var db = firebase.database();
+
+export {
+  app,
+  auth,
+  db
+};
