3db08adc5a39e9147445f59fd72be17e531477e8
Darien Tsai <smartdragondt@gmail.com>
2020-10-03 14:16:56 -0700

    config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -10,6 +10,6 @@ var app = firebase.initializeApp({
     storageBucket: process.env.storageBucket,
     messagingSenderId: process.env.messagingSenderId
 });
-var init = firebase.initializeApp(config);
+var init = firebase.initializeApp();
 
 module.export = {app: app, init: init};
