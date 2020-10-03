f99efd0ec6f9658a5fdff859b65d6e7adfef6bf3
Emaneliforp <kietchouam@gmail.com>
2020-10-03 14:27:59 -0700

    testing fb in test.js

--- src/db/test.js
+++ src/db/test.js
@@ -1,1 +1,15 @@
 console.log('test ran');
+var firebase = require('firebase');
+require('firebase/auth');
+require('firebase/database');
+
+var app = firebase.initializeApp({
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
+});
+
+console.log(firebase);
