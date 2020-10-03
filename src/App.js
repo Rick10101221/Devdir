84c25179f3500f3a7fd1cff2f1bf0d48b7d35b6f
Darien Tsai <smartdragondt@gmail.com>
2020-10-02 18:47:27 -0700

    removed firebase/react/redux hybrid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
-import Init from './firebase/Init';
 
 function App() {
   return (
     <div id="App">
-      <Init/>
       <Dashboard/>
     </div>
   );
