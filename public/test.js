f0c8df53c85380b446720ecd2e0fc186a5ffa1aa
Emaneliforp <kietchouam@gmail.com>
2020-10-03 13:22:35 -0700

    trying to connect to fb

--- public/test.js
+++ public/test.js
@@ -1,1 +1,10 @@
-console.log("hello");
+console.log("working");
+var app = firebase.initializeApp({
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
+});
+var init = firebase.initializeApp(config);
