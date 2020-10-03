40437ae42893226bc27aa328e0b0b69b19db1a97
Emaneliforp <kietchouam@gmail.com>
2020-10-02 18:18:01 -0700

    add firebase js into app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Init from './firebase/init.js';
+import Login from './firebase/login.js';
 
 function App() {
   return (
     <div id="App">
+      <Init/>
+      <Login/>
       <Dashboard/>
     </div>
   );
