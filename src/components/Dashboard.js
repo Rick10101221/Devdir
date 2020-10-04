69d77a468b92472e1e748db903a340c4a1b0a830
unknown <jeffreyliu02@gmail.com>
2020-10-03 18:57:13 -0700

    search result page styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,44 +9,45 @@ import IconButton from '@material-ui/core/IconButton';
 import ChatIcon from '@material-ui/icons/Chat';
 import SearchIcon from '@material-ui/icons/Search';
 import Tooltip from "@material-ui/core/Tooltip";
+import SearchResult from "./SearchResult"
 
 // Dashboard, will contain all subcomponents after login
 class Dashboard extends React.Component{
 
   render(){
+    let button = this.props.chatting ?
+    (<Tooltip title="Go to search">
+    <IconButton
+    variant="contained"
+    color="primary"
+    id="nav-button"
+    size="medium"
+    children={<SearchIcon/>}
+    onClick={this.props.goSearch}
+    ></IconButton></Tooltip>)
+    :
+    (<Tooltip title="Go to messages">
+    <IconButton
+    variant="contained"
+    color="primary"
+    id="nav-button"
+    size="medium"
+    children={<ChatIcon/>}
+    onClick={this.props.goChat}
+    ></IconButton></Tooltip>);
+
+    let page = this.props.chatting ?
+    <ChatPage/>:
+    <SearchPage/>
+
     return(
       <div id="dashboard">
-        {this.props.chatting ?
-        <Tooltip title="Go to search">
-        <IconButton
-        variant="contained"
-        color="primary"
-        id="nav-button"
-        size="medium"
-        children={<SearchIcon/>}
-        onClick={this.props.goSearch}
-        title="Go to messages"
-        ></IconButton></Tooltip>
-        :
-        <Tooltip title="Go to messages">
-        <IconButton
-        variant="contained"
-        color="primary"
-        id="nav-button"
-        size="medium"
-        children={<ChatIcon/>}
-        onClick={this.props.goChat}
-        ></IconButton></Tooltip>
-        }
+        {button}
 
         <Profile/>
         
         <div id="workspace">
-          {
-            this.props.chatting ?
-            <ChatPage/>:
-            <SearchPage/>
-          }
+          <SearchResult/>
           
         </div>
         
